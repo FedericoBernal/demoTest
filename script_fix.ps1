559f260f8d3be471ba742c4af7d4d02058e2ba0e
Matias Lera <62261539+luislera@users.noreply.github.com>
2023-02-08 15:38:44 -0300

    test

--- script_fix.ps1
+++ script_fix.ps1
@@ -1,7 +1,2 @@
-$result = ./Executable/BotTestFramework.Console_fix.exe test --path ${env:FILEPATH} --botId ${env:BOTID} --tenantId ${env:TENANTID}
-
-if (0 -eq $result)
-{
-    Exit 100
-}
-Exit 0
+#./Executable/BotTestFramework.Console_fix.exe test --path ${env:FILEPATH} --botId ${env:BOTID} --tenantId ${env:TENANTID}
+./Executable/BotTestFramework.Console_fix.exe convertChatFile --path ./Tests/basic_transcript.chat --outputFileOption ./Tests/converted.json
