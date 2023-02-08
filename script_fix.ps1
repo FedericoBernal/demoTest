8db35f7bd951312463782bdeffab7e57afc66f22
Matias Lera <62261539+luislera@users.noreply.github.com>
2023-02-08 15:41:04 -0300

    Update script_fix.ps1

--- script_fix.ps1
+++ script_fix.ps1
@@ -1,2 +1,3 @@
 #./Executable/BotTestFramework.Console_fix.exe test --path ${env:FILEPATH} --botId ${env:BOTID} --tenantId ${env:TENANTID}
 ./Executable/BotTestFramework.Console_fix.exe convertChatFile --path ./Tests/basic_transcript.chat --outputFileOption ./Tests/converted.json
+./Executable/BotTestFramework.Console_fix.exe test --path ./Tests/converted.json --botId ${env:BOTID} --tenantId ${env:TENANTID}
