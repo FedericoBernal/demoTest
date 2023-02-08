ba17d5b999e069ceabef567edfda8f7b2c9c5404
LuchoPeres <lucianoperes.dev@gmail.com>
2023-02-08 17:06:16 -0300

    Create chat_to_json.ps1

---
+++ chat_to_json.ps1
@@ -0,0 +1,14 @@
+$inputFolder = ${INPUT}
+$outputFolder = ${OUTPUT}
+
+$chatFiles = Get-ChildItem $inputFolder
+
+foreach ($file in $chatFiles)
+{
+    if ($file.Extension -eq ".chat")
+    {
+        $inputFile = Join-Path $inputFolder $file.Name
+        $outputFile = Join-Path $outputFolder ($file.BaseName + ".json")
+        .\BotTestFramework.Console.exe convertChatFile --path $inputFile --outputFile $outputFile
+    }
+}
