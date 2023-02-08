e2443b7d28e3ae8c0b86ff7bb62f593befd917b5
Matias Lera <62261539+luislera@users.noreply.github.com>
2023-02-08 14:46:22 -0300

    Update script_fix.ps1

--- script_fix.ps1
+++ script_fix.ps1
@@ -1,1 +1,7 @@
-./Executable/BotTestFramework.Console_fix.exe test --path ${env:FILEPATH} --botId ${env:BOTID} --tenantId ${env:TENANTID}
+$result = ./Executable/BotTestFramework.Console_fix.exe test --path ${env:FILEPATH} --botId ${env:BOTID} --tenantId ${env:TENANTID}
+
+if (0 -qa $result)
+{
+    Exit 100
+}
+Exit 0
