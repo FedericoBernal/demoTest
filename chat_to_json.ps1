2c13f13fb2846f551e1d2897ff59207051d20fbd
LuchoPeres <lucianoperes.dev@gmail.com>
2023-02-09 11:31:15 -0300

    added more files, remove prints

--- chat_to_json.ps1
+++ chat_to_json.ps1
@@ -1,9 +1,6 @@
 $inputFolder = ${env:INPUT}
 $outputFolder = ${env:OUTPUT}
 
-Write-Host $inputFolder
-Write-Host $outputFolder
-
 $chatFiles = Get-ChildItem $inputFolder
 
 foreach ($file in $chatFiles)
@@ -13,7 +10,6 @@ foreach ($file in $chatFiles)
         $inputFile = Join-Path $inputFolder $file.Name
         $outputFile = Join-Path $outputFolder ($file.BaseName + ".json")
         .\Executable\BotTestFramework.Console_fix.exe convertChatFile --path $inputFile --outputFileOption $outputFile
-        .\Executable\BotTestFramework.Console_fix.exe test --path $outputFile --botId 53742b3a-1f04-485c-878c-eca03005a462 --tenantId " " --verbose
     }
 }
  
--- single_run.ps1
+++
@@ -1,1 +0,0 @@
-.\Executable\BotTestFramework.Console_fix.exe test --path D:\a\_temp\simple.json --botId 53742b3a-1f04-485c-878c-eca03005a462 --tenantId " " --verbose
