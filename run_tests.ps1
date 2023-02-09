edd78c2f3fbb57b62afe87e0b558692b57424e9a
Matias Lera <62261539+luislera@users.noreply.github.com>
2023-02-09 15:05:23 -0300

    Update run_tests.ps1

--- run_tests.ps1
+++ run_tests.ps1
@@ -1,1 +1,10 @@
-.\Executable\BotTestFramework.Console_fix.exe test --path ${env:OUTPUT} --botId ${env:BOTID} --tenantId ${env:TENANTID} --verbose
+$folder = ${env:OUTPUT}
+
+foreach ($file in Get-ChildItem $folder)
+{
+    if ($file.Extension -eq ".json")
+    {
+        $testFile = Join-Path $folder $file.Name
+        .\Executable\BotTestFramework.Console_fix.exe test --path $testFile --botId ${env:BOTID} --tenantId ${env:TENANTID} --verbose
+    }
+}
