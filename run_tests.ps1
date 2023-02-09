9c30ea0f711e4fb65ad47beb16eddbd23e204aae
LuchoPeres <lucianoperes.dev@gmail.com>
2023-02-09 11:53:07 -0300

    Update run_tests.ps1

--- run_tests.ps1
+++ run_tests.ps1
@@ -1,1 +1,1 @@
-.\Executable\BotTestFramework.Console_fix.exe test --path D:\a\_temp\ --botId 53742b3a-1f04-485c-878c-eca03005a462 --tenantId " " --verbose
+.\Executable\BotTestFramework.Console_fix.exe test --path $(env:OUTPUT) --botId $(env:BOTID) --tenantId $(env:TENANTID) --verbose
